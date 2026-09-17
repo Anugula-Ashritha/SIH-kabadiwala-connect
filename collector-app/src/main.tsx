697f490aa87681d2ffc7f3f03e7609ebd5a95557
Anugula-Ashritha <ashrithaofficialacc@gmail.com>
2026-09-17 13:01:18 +0530

    feat: make collector deployment the SIH entry point

--- collector-app/src/main.tsx
+++ collector-app/src/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {RoleSelectionScreen} from './screens/RoleSelectionScreen';
 import './index.css';
+
+const isCollectorMode = new URLSearchParams(window.location.search).get('role') === 'collector';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isCollectorMode ? <App /> : <RoleSelectionScreen />}
   </StrictMode>,
 );
